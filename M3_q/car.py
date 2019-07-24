8b497e7b19ee1b0aa18cafec1b3420969b2e4e11
sandhya2408 <sandhyabhagavath@gmail.com>
2019-07-24 11:33:41 +0530

    commit

--- M3_q/car.py
+++ M3_q/car.py
@@ -2,9 +2,29 @@ class Car:
     def __init__(self, regno, no_gears):
         self.regno = regno
         self.no_gears = no_gears
+        self.is_started = False
+        self.c_gear = 0
     def start(self):
-        print(f"car with regno: {self.regno} started....")
+        if self.is_started:
+            print(f"car with regno:{self.regno} already started")
+        else:
+            print(f"car with regno: {self.regno} started....")
+            self.is_started = True
     def stop(self):
-          print(f"car with regno: {self.regno} stopped....")
+        if self.is_started:
+            print(f"car with regno: {self.regno} stopped....")
+            self.is_started = False
+        else:
+            print(f"car with regno:{self.regno} already stopped")
+
     def change_gear(self):
-          print(f"car with regno: {self.regno} changed gear....")
+        if self.is_started:
+            if self.c_gear < self.no_gears:
+                self.c_gear += 1
+                print(f"car with regno: {self.regno} changed gear....")
+            else:
+                print(f"car with regno: {self.regno} cant change the gear")
+        else:
+            print(f"car has stopped so cant change gears")
+    def showInfo(self):
+        print( f"car started with regno:{self.regno} {self.is_started} no of gears:{self.no_gears} gear status:{self.c_gear}")
--- M3_q/managercar.py
+++ M3_q/managercar.py
@@ -1,6 +1,17 @@
 from car import Car
-if __name__ == "__main__":
-    bmw = Car("KA20MA3307",5)
-    bmw.start()
-    bmw.change_gear()
-    bmw.stop()
+car1 = Car("KA20MA3307",5)
+car2 = Car("KA20MA3308",4)
+car3 = Car("KA20MA3309",5)
+car4 = Car("KA20MA3303",6)
+car5 = Car("KA20MA3302",4)
+car1.start()
+car2.start()
+car3.start()
+car1.change_gear()
+car2.change_gear()
+car1.change_gear()
+lst = [car1,car2,car3,car4,car5]
+for car in lst:
+    car.showInfo()
+c = len(list(filter(lambda x : x.is_started and x.c_gear==0,lst)))
+print(c)
