bec1e3c091c30b295a40dbf435c31389618051d7
sandhya2408 <sandhyabhagavath@gmail.com>
2019-07-26 16:17:15 +0530

    commit

--- recbin.py
+++ recbin.py
@@ -1,21 +1,20 @@
-def binarySearch(lst,key):
-    l = 0
-    h = len(lst) - 1
+
+def binarySearch(lst,l,h,key):
+ 
     while l <= h:
         mid = (l + h ) // 2
         if lst[mid] == key:
             return mid
         elif lst[mid] < key:
-            l = mid + 1
+            return binarySearch(lst,mid+1 ,h,key)
         else:
-            h = mid - 1
+            return binarySearch(lst,l,mid-1,key)
     return -1
 
 
 
-
 ele = 6
-res = binarySearch([1,2,3,4,5,6,7,8,9,10],ele)
+res = binarySearch([1,2,3,4,5,6,7,8,9,10],0,7,ele)
 if res == -1:
     print(f"{ele} is not found")
 else:
